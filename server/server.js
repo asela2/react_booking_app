66739e1ded76721e7152c8c689f843a2e38ab8d7
asela2 <aselajayathilake2@gmail.com>
2023-01-12 08:42:20 +0530

    setup initial express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log("server running");
+const express = require("express");
+
+const app = express();
+
+app.get("/api/:message", (req, res) => {
+  res.status(200).send(req.params.message);
+});
+
+app.listen(5000, () => console.log("server running on port 8000"));
