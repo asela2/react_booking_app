45b58121f6e2026e71db86cbb7b839d0b2f9a71b
asela2 <aselajayathilake2@gmail.com>
2023-01-13 08:19:15 +0530

    created initial auth route file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const express = require("express");
+import express from "express";
+import router from "./routes/auth";
 
 const app = express();
 
-app.get("/api/:message", (req, res) => {
-  res.status(200).send(req.params.message);
-});
+//route middleware
+app.use("/api", router);
 
 app.listen(5000, () => console.log("server running on port 8000"));
